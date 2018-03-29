783d169d4e3a2570c201e2615e40ddb5b3dbcdf4
Sam <sam.noedel@gmail.com>
2018-03-29 07:56:10 -0700

    Change chunk crop return & implement can_output

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,24 @@
 extern crate byteorder;
 use byteorder::{BigEndian,ByteOrder};
 
+struct Png<'a> {
+    data: &'a [u8]
+}
+
+impl<'a> Png<'a> {
+    fn new(data: &[u8]) -> Png {
+        Png { data }
+    }
+
+    fn parts(&self) -> (PngHeader, PngChunks<'a>) {
+        (PngHeader, PngChunks::new(&self.data[8..]))
+    }
+
+    fn validate(&self) {
+        assert_eq!(self.data, PngHeader.as_ref());
+    }
+}
+
 struct PngHeader;
 
 impl AsRef<[u8]> for PngHeader {
@@ -27,9 +45,10 @@ impl<'a> Iterator for PngChunks<'a> {
     type Item = Chunk<'a>;
 
     fn next(&mut self) -> Option<Chunk<'a>> {
-        let size = BigEndian::read_u32(&self.data[self.cur..]) as usize;
+        let mut size = BigEndian::read_u32(&self.data[self.cur..]) as usize;
+        size += 12;
         let dat = &self.data[self.cur..self.cur + size];
-        self.cur += size + 12;
+        self.cur += size;
         Some(Chunk { dat })
     }
 }
@@ -64,20 +83,6 @@ impl<'a> AsRef<[u8]> for Chunk<'a> {
     }
 }
 
-struct Png<'a> {
-    data: &'a [u8]
-}
-
-impl<'a> Png<'a> {
-    fn new(data: &[u8]) -> Png {
-        Png { data }
-    }
-
-    fn parts(&self) -> (PngHeader, PngChunks<'a>) {
-        (PngHeader, PngChunks::new(&self.data[8..]))
-    }
-}
-
 pub struct Rect {
     x: u32,
     y: u32,
@@ -87,21 +92,18 @@ pub struct Rect {
 
 pub fn crop<T: AsRef<[u8]>>(input: T, rect: Rect, output: &mut Vec<u8>) {
     let png = Png::new(input.as_ref());
+    png.validate();
     let (header, chunks) = png.parts();
 
     // Write header to output
-    header
-        .as_ref()
-        .iter()
-        .cloned()
-        .for_each(|b| output.push(b));
+    output.extend_from_slice(header.as_ref());
 
     // Write chunks to output
     chunks
         .filter_map(|chunk| {
-            if let false = can_output(&chunk) {
-                let cropped = crop_chunk(chunk, &rect);
-                Some(cropped)
+            if can_output(&chunk) {
+                let cropped = crop_chunk(&chunk, &rect);
+                Some(chunk)
             } else {
                 None
             }
@@ -109,14 +111,15 @@ pub fn crop<T: AsRef<[u8]>>(input: T, rect: Rect, output: &mut Vec<u8>) {
         .for_each(|chunk| {
             output.extend_from_slice(chunk.as_ref());
         });
+    output.shrink_to_fit();
 }
 
 fn can_output(chunk: &Chunk) -> bool {
-    true
+    chunk.typ() & 0x0F == 0x0F
 }
 
-fn crop_chunk<'a>(chunk: Chunk<'a>, _rect: &Rect) -> Chunk<'a> {
-    chunk
+fn crop_chunk<'a>(_chunk: &Chunk<'a>, _rect: &Rect) -> Vec<u8> {
+    vec!()
 }
 
 #[cfg(test)]
